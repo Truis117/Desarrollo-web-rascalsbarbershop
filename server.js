4b49035a34f961af294fae7dc965086f38902c6d
Luis Hernandez <hernandezgalvis830@gmail.com>
2025-11-06 11:05:11 -0500

    refactor: mejora de estructura de archivos

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Servir archivos estáticos desde public/
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname)));
 
 // Routes
 app.get('/', (req, res) => res.render('index'));
